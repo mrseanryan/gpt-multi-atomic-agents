610199c35d2c1513e6322b61634a8458e7505e41
Sean Ryan <str_ie@yahoo.co.uk>
2024-12-13 23:50:55 +0100

    TS update

--- clients/gpt-maa-ts/src/test_gpt_maa_client.ts
+++ clients/gpt-maa-ts/src/test_gpt_maa_client.ts
@@ -3,19 +3,31 @@
 // - Agents can be configured to understand a subset of each others output
 // - Because the service uses a Blackboard, the agents are then able to collaborate together, since they understand a subset of one another's output.
 
-import { execute } from "./function_call_executor.js"
-import { handleUserPrompt } from "./index.js"
-import { agentDefinitions, chatAgentDescription, functionRegistry } from "./resources_test_domain.js"
-import { dumpJson, print, printDetail } from "./utils_print.js"
+import { execute } from "./function_call_executor.js";
+import { handleUserPrompt } from "./index.js";
+import {
+  agentDefinitions,
+  chatAgentDescription,
+  functionRegistry,
+} from "./resources_test_domain.js";
+import { dumpJson, print, printDetail } from "./utils_print.js";
 
 // =================================================
 // Chat with the Agents
 
-await handleUserPrompt("What can you do?", agentDefinitions, chatAgentDescription)
-const blackboardAccessor = await handleUserPrompt("Mow the lawn, dealing with any lawn furniture and waste. After mowing make sure waste is disposed of.", agentDefinitions, chatAgentDescription)
+await handleUserPrompt(
+  "What can you do?",
+  agentDefinitions,
+  chatAgentDescription
+);
+const blackboardAccessor = await handleUserPrompt(
+  "mow lawn and clear waste. before that, move furniture away, and at the end, restore the furniture.",
+  agentDefinitions,
+  chatAgentDescription
+);
 
 if (!blackboardAccessor) {
-    throw new Error("No blackboard accessor was returned!")
+  throw new Error("No blackboard accessor was returned!");
 }
 
 // =================================================
@@ -25,11 +37,16 @@ dumpJson(messages);
 
 // =================================================
 // Execute the Function Calls using our Handlers
-blackboardAccessor.get_new_functions()
+blackboardAccessor.get_new_functions();
 const onExecuteStart = async () => {
-    printDetail("(execution started)")
-}
+  printDetail("(execution started)");
+};
 const onExecuteEnd = async () => {
-    printDetail("(execution ended)")
-}
-await execute(blackboardAccessor.get_new_functions(), functionRegistry, onExecuteStart, onExecuteEnd);
+  printDetail("(execution ended)");
+};
+await execute(
+  blackboardAccessor.get_new_functions(),
+  functionRegistry,
+  onExecuteStart,
+  onExecuteEnd
+);
